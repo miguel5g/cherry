84fafb661f79cb06cc752d5f070d63005893a72b
Miguel Ângelo <miguelcg958@gmail.com>
2024-01-11 07:53:06 -0300

    WIP: improve feed page

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,7 +1,110 @@
+import { Button } from '@/components/ui/button';
+import { ChevronsUpIcon, ReplyIcon, ShareIcon } from 'lucide-react';
+
 function Feed() {
   return (
-    <div>
-      <h1>Página inicial (Feed)</h1>
+    <div className="px-8 py-16">
+      <main className="flex flex-col max-w-2xl gap-16 mx-auto">
+        <div>
+          <div className="flex items-center gap-2">
+            <div className="w-12 h-12 rounded-full bg-muted" />
+            <div>
+              <p>Miguel Ângelo</p>
+              <p className="-mt-1.5 text-muted-foreground text-sm">@miguel5g</p>
+            </div>
+            <div className="w-1.5 h-1.5 bg-muted-foreground rounded-full" />
+            <span className="text-sm text-muted-foreground">há 7 horas</span>
+          </div>
+
+          <p className="mt-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, minima dolorum
+            perferendis necessitatibus ratione assumenda temporibus repudiandae molestiae sequi
+            saepe distinctio, incidunt vel. Veniam officia quo eveniet reprehenderit mollitia
+            tempore!
+          </p>
+
+          <div className="flex items-center gap-1 mt-4">
+            <Button variant="ghost">
+              <ChevronsUpIcon size={16} />
+              <span>1.2k</span>
+            </Button>
+            <Button variant="ghost">
+              <ReplyIcon size={16} />
+              <span>Responder</span>
+            </Button>
+            <Button variant="ghost">
+              <ShareIcon size={16} />
+              <span>Compartilhar</span>
+            </Button>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-2">
+            <div className="w-12 h-12 rounded-full bg-muted" />
+            <div>
+              <p>Miguel Ângelo</p>
+              <p className="-mt-1.5 text-muted-foreground text-sm">@miguel5g</p>
+            </div>
+            <div className="w-1.5 h-1.5 bg-muted-foreground rounded-full" />
+            <span className="text-sm text-muted-foreground">há 7 horas</span>
+          </div>
+
+          <p className="mt-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, minima dolorum
+            perferendis necessitatibus ratione assumenda temporibus repudiandae molestiae sequi
+            saepe distinctio, incidunt vel. Veniam officia quo eveniet reprehenderit mollitia
+            tempore!
+          </p>
+
+          <div className="flex items-center gap-1 mt-4">
+            <Button variant="ghost">
+              <ChevronsUpIcon size={16} />
+              <span>1.2k</span>
+            </Button>
+            <Button variant="ghost">
+              <ReplyIcon size={16} />
+              <span>Responder</span>
+            </Button>
+            <Button variant="ghost">
+              <ShareIcon size={16} />
+              <span>Compartilhar</span>
+            </Button>
+          </div>
+        </div>
+        <div>
+          <div className="flex items-center gap-2">
+            <div className="w-12 h-12 rounded-full bg-muted" />
+            <div>
+              <p>Miguel Ângelo</p>
+              <p className="-mt-1.5 text-muted-foreground text-sm">@miguel5g</p>
+            </div>
+            <div className="w-1.5 h-1.5 bg-muted-foreground rounded-full" />
+            <span className="text-sm text-muted-foreground">há 7 horas</span>
+          </div>
+
+          <p className="mt-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, minima dolorum
+            perferendis necessitatibus ratione assumenda temporibus repudiandae molestiae sequi
+            saepe distinctio, incidunt vel. Veniam officia quo eveniet reprehenderit mollitia
+            tempore!
+          </p>
+
+          <div className="flex items-center gap-1 mt-4">
+            <Button variant="ghost">
+              <ChevronsUpIcon size={16} />
+              <span>1.2k</span>
+            </Button>
+            <Button variant="ghost">
+              <ReplyIcon size={16} />
+              <span>Responder</span>
+            </Button>
+            <Button variant="ghost">
+              <ShareIcon size={16} />
+              <span>Compartilhar</span>
+            </Button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
